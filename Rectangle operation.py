01cc5d9118ac5815d0e6e1dc7a5a855cd029d43d
Omprakasdash <lapomdash@gmail.com>
2024-07-25 23:08:33 +0530

    Add files via upload

---
+++ Rectangle operation.py
@@ -0,0 +1,7 @@
+# Calculate the area and perimeter of a rectangle by taking the input from the user?
+
+a=float(input("Enter the value for length of the rectange:"))
+b=float(input("Enter the value for breath of the rectange:"))
+print("The area and perimeter of the rectangle is {}*{}={} and 2*({}+{})={} respectively".format(a,b,a*b,a,b,2*(a+b)))
+
+
