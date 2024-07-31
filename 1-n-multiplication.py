0483b0ef2d7398c9dc7c82f9926f855c85a6e0da
Omprakasdash <lapomdash@gmail.com>
2024-08-01 00:27:17 +0530

    Another file is added

--- 1-n-multiplication.py
+++ 1-n-multiplication.py
@@ -3,7 +3,7 @@ print("Method:-1 :-Using range()")
 n=int(input("Enter the number"))
 for i in range(1,(n+1)):
     for j in range(1,11):
-        print("{}*{}={}".format(i,j,i*j)) 
+        print("{}*{}={}".format(i,j,i*j))
 
 print("Method:-2:By using while loop")
         
