8693e7c35e4b17a6a88cadd8ca79f074ad8097ca
Om <lapomdash@gmail.com>
2024-08-01 08:43:42 +0530

    This is my new program

---
+++ multiplication-table-of-list.py
@@ -0,0 +1,8 @@
+n=input("Enter the numbers").split()
+print(n,type(n))
+for i in n:
+    x=int(i)
+    print(x,type(x))
+    print(i,type(i))
+    for j in range(1,11):
+        print("{}*{}={}".format(x,j,x*j))
