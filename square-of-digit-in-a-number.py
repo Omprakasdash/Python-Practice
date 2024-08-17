ca2d007753a5817285bd105823ab474a6743d3a6
Om <lapomdash@gmail.com>
2024-08-17 07:35:50 +0530

    File is commited

---
+++ square-of-digit-in-a-number.py
@@ -0,0 +1,7 @@
+n=input("Enter the number : ")
+s=0
+for i in n:
+    a=list(i)
+    for j in a:
+        print(j,type(j),len(j))
+        print(a,type(a),len(a))
---
+++ sum-of-digit-in-a-number.py
@@ -0,0 +1,12 @@
+# Find the sum of digits in a number. 
+n=input("Enter the number :")
+print(n,type(n))
+s=0
+for i in n :
+    # print(i,type(i))
+    i=int(i)
+    print(i,type(i))
+    s+=i
+print(s)
+
+
